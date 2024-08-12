7eae06d868b0c0c0c70fee8d0cdada0bf6b6d627
cemil3535 <cemilozcan35@gmail.com>
2024-08-13 00:23:59 +0300

    Add project files.

---
+++ AbstractionPractice/Employees.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractionPractice
+{
+    public abstract class Employees
+    {
+        // Property to identify
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Department { get; set; }
+
+       
+        // abstract metod created
+        public abstract void Duty();
+
+    }
+
+    // project manager inherited employees
+    public  class ProjectManager : Employees
+    {
+        
+
+        // abstract metod override 
+        public override void Duty()
+        {
+            Console.WriteLine($"{Name} {Surname} {Department} Proje Yoneticisi olarak calisiyorum."); 
+        }
+    }
+
+    // softwareDeveloper inherited employees
+    public class SoftwareDeveloper : Employees
+    {
+       
+
+        // abstract metod override 
+        public override void Duty()
+        {
+            Console.WriteLine($"{Name} {Surname} {Department} Yazilim Gelistirici olarak calisiyorum");
+        }
+    }
+
+    // salesRepresentative inherited employees
+    public class SalesRepresentative : Employees
+    {
+       
+
+        // abstract metod override 
+        public override void Duty() 
+        {
+            Console.WriteLine($"{Name} {Surname} {Department} Satis Temsilcisi olarak calisiyorum");
+        }
+    }
+
+}
---
+++ AbstractionPractice/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AbstractionPractice;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // object created 
+        ProjectManager manager = new ProjectManager()
+        {
+            Name = "Hasan",
+            Surname = "Cildirmis",
+            Department = "ARGE"
+        };
+        // Duty abstract metod call
+        manager.Duty();
+    }
+}
